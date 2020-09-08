ca7bcfeb582f0538ab4d2a11f1d53f90b414d345
juliannalim <julianna.j.lim@gmail.com>
2020-09-08 00:13:26 -0400

    test functions work

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-export default function (props) {
+export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
@@ -17,9 +17,17 @@ export default function (props) {
     reset();
   };
 
-  const validate = () => {
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    setError("");
     props.onSave(name, interviewer);
   }
+
+  const [error, setError] = useState("");
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -34,9 +42,14 @@ export default function (props) {
             onChange={(e) => {
               setName(e.target.value)
             }}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
-        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
+        <InterviewerList
+          interviewers={props.interviewers}
+          interviewer={interviewer}
+          onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
