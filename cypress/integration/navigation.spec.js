2514f5d8a1b31ccabb77a74c233174abdec3bfa2
juliannalim <julianna.j.lim@gmail.com>
2020-09-09 14:47:21 -0400

    cypress set up and using it for testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.css", "day-list__item--selected");
+  });
+
+});
